8f17e79ca78a0ca1e836299aaeb13b9d622702a5
Mateus Muller <mateusmuller@github.com>
2024-11-08 08:23:23 -0300

    feat: remote backend

--- kubernetes/02_kubernetes_production_ready/terraform/provider.tf
+++ kubernetes/02_kubernetes_production_ready/terraform/provider.tf
@@ -4,6 +4,25 @@ terraform {
       source  = "Telmate/proxmox"
       version = "3.0.1-rc4"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+  backend "s3" {
+    bucket = "proxmox-state"
+    key    = "terraform.tfstate"
+    region = "placeholder"
+
+    endpoints = {
+      s3 = "http://minio:9000"
+    }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    force_path_style            = true
   }
 }
 
